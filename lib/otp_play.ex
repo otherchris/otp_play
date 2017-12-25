a82b69ea0e9e79fe7b6dc363c779a4351e94c295
Chris Caragianis <chriscaragianis@gmail.com>
2017-12-25 11:50:52 -0500

    initial commit

---
+++ lib/otp_play.ex
@@ -0,0 +1,18 @@
+defmodule OtpPlay do
+  @moduledoc """
+  Documentation for OtpPlay.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OtpPlay.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/otp_play_test.exs
@@ -0,0 +1,8 @@
+defmodule OtpPlayTest do
+  use ExUnit.Case
+  doctest OtpPlay
+
+  test "greets the world" do
+    assert OtpPlay.hello() == :world
+  end
+end
